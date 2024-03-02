3c0b48cc5a61ebf258389bea6c75206f22e9dd0d
steadiness <tlsgus92wns@naver.com>
2024-03-02 18:43:57 +0900

    Initial commit

---
+++ src/main/java/com/project/baedang/BaedangApplication.java
@@ -0,0 +1,13 @@
+package com.project.baedang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaedangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/baedang/BaedangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.baedang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
